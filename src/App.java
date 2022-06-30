aebee5349a81b072c8dec119552d921faec0dbea
Pasan Dewasurendra <pasandewasurendra@gmail.com>
2022-06-30 10:12:32 +0530

    Added createColorGrid and showColorGrid functions

--- src/App.java
+++ src/App.java
@@ -1,5 +1,54 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class App {
+
+    private static List<List<Node>> colorGrid = new ArrayList<>();
+
+    public static void createColorGrid(int col, int row){
+        List<String> colorList = new ArrayList<>(Arrays.asList("blue", "red", "green"));
+
+        for(int i=0; i<row; i++){
+            List<Node> gridRow = new ArrayList<>();
+
+            for(int j=0; j<col; j++){
+
+                int randomIndex = (int) (Math.random() * colorList.size());
+                String randomColor = colorList.get(randomIndex);
+
+                Node node = new Node(randomColor, i, j);
+                gridRow.add(node);
+            }
+            colorGrid.add(gridRow);
+        }
+    }
+
+    public static void showColorGrid(){
+
+        for (List<Node> row : colorGrid){
+            for (Node node : row){
+                System.out.print(String.format("%-8s", node.getColor().toUpperCase()));
+            }
+            System.out.println();
+        }
+    }
+
+    public static void findLargestBlock(){
+
+        List<Node> largestBlocks = new ArrayList<>();
+
+        for(List<Node> row : colorGrid){
+            for(Node current : row){
+
+            }
+        }
+    }
+
     public static void main(String[] args) {
+
+        createColorGrid(5,5);
+        showColorGrid();
 
     }
 }
--- src/Node.java
+++ src/Node.java
@@ -1,2 +1,95 @@
 public class Node {
+    private String color;
+    private int x;
+    private int y;
+    private Node neighbourLeft;
+    private Node neighbourRight;
+    private Node neighbourTop;
+    private Node neighbourBottom;
+
+    public Node(String color, int x, int y) {
+        this.color = color;
+        this.x = x;
+        this.y = y;
+    }
+
+    public Node(String color, int x, int y, Node neighbourLeft, Node neighbourRight, Node neighbourTop, Node neighbourBottom) {
+        this.color = color;
+        this.x = x;
+        this.y = y;
+        this.neighbourLeft = neighbourLeft;
+        this.neighbourRight = neighbourRight;
+        this.neighbourTop = neighbourTop;
+        this.neighbourBottom = neighbourBottom;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Node getNeighbourLeft() {
+        return neighbourLeft;
+    }
+
+    public void setNeighbourLeft(Node neighbourLeft) {
+        this.neighbourLeft = neighbourLeft;
+    }
+
+    public Node getNeighbourRight() {
+        return neighbourRight;
+    }
+
+    public void setNeighbourRight(Node neighbourRight) {
+        this.neighbourRight = neighbourRight;
+    }
+
+    public Node getNeighbourTop() {
+        return neighbourTop;
+    }
+
+    public void setNeighbourTop(Node neighbourTop) {
+        this.neighbourTop = neighbourTop;
+    }
+
+    public Node getNeighbourBottom() {
+        return neighbourBottom;
+    }
+
+    public void setNeighbourBottom(Node neighbourBottom) {
+        this.neighbourBottom = neighbourBottom;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Node{");
+        sb.append("color='").append(color).append('\'');
+        sb.append(", x=").append(x);
+        sb.append(", y=").append(y);
+        sb.append(", neighbourLeft=").append(neighbourLeft);
+        sb.append(", neighbourRight=").append(neighbourRight);
+        sb.append(", neighbourTop=").append(neighbourTop);
+        sb.append(", neighbourBottom=").append(neighbourBottom);
+        sb.append('}');
+        return sb.toString();
+    }
 }
